e6c3f474401e08b952b4054c6f789745b0afc9c6
Mattias E. O. Andersson <adelost@gmail.com>
2024-06-16 07:27:36 +0200

    omw

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 import streamlit as st
+import plotly.express as px
 import plotly.graph_objs as go
 
 CSV_FILE_PATH = './src/weather_data.csv'
@@ -36,28 +37,34 @@ data_tables = {
 placeholder = st.empty()
 with placeholder.container():
     col1, col2 = st.columns([1, 1])  # Adjust column widths
-    with col1:
-        pass
-    with col2:
-        pass
-    st.line_chart(data_tables['wind'].set_index('timestamp'))
-    st.line_chart(data_tables['temperature'].set_index('timestamp'))
-    col3, col4 = st.columns([3, 3])  # Adjust column widths
-    # with col3:
-    # st.line_chart(data_tables['windDegrees'].set_index('timestamp'))
-    if not data_tables['windDegrees'].empty:
-        latest_wind_deg = data_tables['windDegrees']['windDegrees'].iloc[-1]
-        fig = go.Figure(go.Scatterpolar(
-            r=[1],
-            theta=[latest_wind_deg],
-            mode='markers',
-            marker=dict(size=14, color='red')
-        ))
 
-        fig.update_layout(
-            polar=dict(
-                radialaxis=dict(visible=False),
-            ),
-            showlegend=False
-        )
-        st.plotly_chart(fig)
+    # Wind Line Chart
+    fig_wind = px.line(data_tables['wind'], x='timestamp', y=['windAvg', 'windMin', 'windMax'], title='Wind Speed Over Time')
+    st.plotly_chart(fig_wind, use_container_width=True)
+
+    # Temperature Line Chart
+    fig_temp = px.line(data_tables['temperature'], x='timestamp', y='temperature', title='Temperature Over Time')
+    st.plotly_chart(fig_temp, use_container_width=True)
+
+
+
+    col3, col4 = st.columns([3, 3])  # Adjust column widths
+    with col3:
+        # Wind Degrees Polar Plot
+        if not data_tables['windDegrees'].empty:
+            latest_wind_deg = data_tables['windDegrees']['windDegrees'].iloc[-1]
+            fig_polar = go.Figure(go.Scatterpolar(
+                r=[1],
+                theta=[latest_wind_deg],
+                mode='markers',
+                marker=dict(size=14, color='red')
+            ))
+
+            fig_polar.update_layout(
+                polar=dict(
+                    radialaxis=dict(visible=False),
+                ),
+                showlegend=False,
+                title='Wind Direction'
+            )
+            st.plotly_chart(fig_polar, use_container_width=True)
